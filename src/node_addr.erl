76c4ac288789377dce6fa8d9ab8985799e877b84
Essien Ita Essien <essiene@gmail.com>
2008-11-20 22:23:52 +0100

    Flesh out node_addr module with some more manipulation helpers
    - Implement parent/1 and basename/1 for address manipulation,
      they behave like named (think POSIX path manipulation).
    
    - Add unittests for node_addr (and catch corner case bug in node_addr:join/1)
    
    - Begin to order my thoughts concerning the interpreter, in the docs

--- src/node_addr.erl
+++ src/node_addr.erl
@@ -1,8 +1,32 @@
 -module(node_addr).
 -export([
-        split/1,
-        join/1
+        basename/1,
+        parent/1
     ]).
+
+-export([
+        join/1,
+        split/1
+    ]).
+
+basename(NodeAddr) when is_atom(NodeAddr) ->
+    {_Parent, Child} = emancipate(NodeAddr),
+    Child.
+
+parent(NodeAddr) when is_atom(NodeAddr) ->
+    {Parent, _Child} = emancipate(NodeAddr),
+    Parent.
+
+join([]) ->
+    '';
+join([H | _Rest]=NodeAddrList) when is_list(NodeAddrList), is_atom(H) ->
+    AtomList2StrList = fun (Item) ->
+            atom_to_list(Item)
+    end,
+
+    StrList = lists:map(AtomList2StrList, NodeAddrList),
+    StrAddr = string:join(StrList, "."),
+    list_to_atom(StrAddr).
 
 
 split(NodeAddr) when is_atom(NodeAddr) ->
@@ -15,13 +39,11 @@ split(NodeAddr) when is_atom(NodeAddr) ->
 
     lists:map(StrList2AtomList, StrList).
 
-join([H | _Rest]=NodeAddrList) when is_list(NodeAddrList), is_atom(H) ->
-    AtomList2StrList = fun (Item) ->
-            atom_to_list(Item)
-    end,
 
-    StrList = lists:map(AtomList2StrList, NodeAddrList),
-    StrAddr = string:join(StrList, "."),
-    list_to_atom(StrAddr).
 
+
+emancipate(NodeAddr) when is_atom(NodeAddr) ->
+    List = split(NodeAddr),
+    {ParentList, [Child]} = lists:split(length(List) - 1, List),
+    {join(ParentList), Child}.
 
---
+++ tests/test_node_addr.erl
@@ -0,0 +1,26 @@
+-module(test_node_addr).
+-include_lib("eunit/include/eunit.hrl").
+
+
+basename_test() ->
+    ?assertEqual(baz, node_addr:basename(foo.bar.baz)),
+    ?assertEqual(bar, node_addr:basename(foo.bar)),
+    ?assertEqual(foo, node_addr:basename(foo)).
+
+parent_test() ->
+    ?assertEqual(foo.bar, node_addr:parent(foo.bar.baz)),
+    ?assertEqual(foo, node_addr:parent(foo.bar)),
+    ?assertEqual('', node_addr:parent(foo)).
+
+join_test() ->
+    ?assertEqual('', node_addr:join([])),
+    ?assertEqual(foo, node_addr:join([foo])),
+    ?assertEqual(foo.bar, node_addr:join([foo, bar])),
+    ?assertEqual(foo.bar.baz, node_addr:join([foo, bar, baz])).
+
+
+split_test() ->
+    ?assertEqual([], node_addr:split('')),
+    ?assertEqual([foo], node_addr:split(foo)),
+    ?assertEqual([foo, bar], node_addr:split(foo.bar)),
+    ?assertEqual([foo, bar, baz], node_addr:split(foo.bar.baz)).
