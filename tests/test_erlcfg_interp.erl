865086cb39751ef21b8524d3e31d5ee502ec947f
Essien Ita Essien <essiene@gmail.com>
2008-11-22 16:57:50 +0100

    Unittest eval for {val, Data, _Ignore}

--- tests/test_erlcfg_interp.erl
+++ tests/test_erlcfg_interp.erl
@@ -4,3 +4,13 @@
 
 new_test() ->
     ?assertEqual([], erlcfg_interp:new()).
+
+eval_val_with_empty_interp_test() ->
+    Interp = erlcfg_interp:new(),
+    Expected = {Interp, foo},
+    ?assertEqual(Expected, erlcfg_interp:eval(Interp, {val, foo, nil})).
+
+eval_val_with_nonempty_interp_test() ->
+    Interp = [{foo, bar}, {bar, baz}],
+    Expected = {Interp, 5},
+    ?assertEqual(Expected, erlcfg_interp:eval(Interp, {val, 5, nil})).
