5c624c7c58a6667581c3b0bff38f7b655dd94b9c
Essien Ita Essien <essiene@gmail.com>
2008-11-22 16:47:40 +0100

    Start interpreter implementation.
    Pretty simple at this stage... just three evals, which will
    continue to grow, but hopefully, not by much.
    
    Next... to test this.

---
+++ src/erlcfg_interp.erl
@@ -0,0 +1,28 @@
+-module(erlcfg_interp).
+-export([
+        new/0,
+        eval/2
+    ]).
+
+
+new() ->
+    erlcfg_node:new().
+
+eval(State, {val, Data, _Ignore}) ->
+    {State, Data};
+
+eval(State, {get, Address, _Ignore}) ->
+   case erlcfg_node:get(State, Address) of
+       {State, Value} ->
+           {State, Value};
+       {not_found, InvalidAddress} ->
+           throw({not_found, InvalidAddress})
+   end;
+
+eval(State, {set, Address, Value}) ->
+    case erlcfg_node:set(State, Address, Value) of
+        {State, Value} ->
+            {State, Value};
+        {not_found, InvalidAddress} ->
+            throw({not_found, InvalidAddress})
+    end.
