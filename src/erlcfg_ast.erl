fb2f7948d8040c11d5d1e690a3690f575af7dde0
Essien Ita Essien <essiene@gmail.com>
2008-11-25 08:13:48 +0100

    Add untested traversal code. Make it build

--- src/erlcfg_ast.erl
+++ src/erlcfg_ast.erl
@@ -1,3 +1,15 @@
 -module(erlcfg_ast).
 -export([
+        traverse/1
     ]).
+
+
+traverse(Ast) ->
+    traverse(Ast, erlcfg_interp:new()).
+
+traverse([], Interp) ->
+    Interp;
+
+traverse([Command | Rest], Interp) ->
+    {NewInterp, _Value} = erlcfg_interp:eval(Interp, Command),
+    traverse(Rest, NewInterp).
