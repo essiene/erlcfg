0ae99749e8b21de93bc3fc131f421dd6a2a7d9dc
Essien Ita Essien <essiene@gmail.com>
2008-11-25 08:27:09 +0100

    Start unittesting. Fix traversal to properly decend nested Asts

--- src/erlcfg_ast.erl
+++ src/erlcfg_ast.erl
@@ -10,6 +10,10 @@ traverse(Ast) ->
 traverse([], Interp) ->
     Interp;
 
-traverse([Command | Rest], Interp) ->
+traverse([Command | Rest], Interp) when is_tuple(Command) ->
     {NewInterp, _Value} = erlcfg_interp:eval(Interp, Command),
+    traverse(Rest, NewInterp);
+
+traverse([Ast | Rest], Interp) when is_list(Ast) ->
+    NewInterp = traverse(Ast, Interp),
     traverse(Rest, NewInterp).
