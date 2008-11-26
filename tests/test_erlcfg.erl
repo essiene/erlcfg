abbfddf953bffba7a4e6b5a91428bb6e28fb3342
Essien Ita Essien <essiene@gmail.com>
2008-11-26 12:18:39 +0100

    Test end-to-end with a more complex file. Passes baby!!!

--- tests/test_erlcfg.erl
+++ tests/test_erlcfg.erl
@@ -13,3 +13,24 @@ new_variable_test() ->
     Config = erlcfg:new("flat.conf"),
     ?assertEqual(1, Config:get(six_variable)),
     ?assertEqual(awesome@quoted.atom, Config:get(seven_variable)).
+
+nested_file_test() ->
+    Config = erlcfg:new("full.conf"),
+    ?assertEqual("www.appserver.com", Config:get(common.appserver)),
+    ?assertEqual(5038, Config:get(common.port.ami)),
+    ?assertEqual(9119, Config:get(common.port.rest)),
+
+    ?assertEqual("0.0.0.0", Config:get(general.listen)),
+    ?assertEqual(9119, Config:get(general.port)),
+    ?assertEqual(2, Config:get(general.wait.short)),
+    ?assertEqual(10, Config:get(general.wait.long)),
+
+    ?assertEqual("www.appserver.com", Config:get(ami.host)),
+    ?assertEqual(5038, Config:get(ami.port)),
+    ?assertEqual("user", Config:get(ami.username)),
+    ?assertEqual("pass", Config:get(ami.secret)),
+
+    ?assertEqual(5, Config:get(callentry.rttl)),
+    ?assertEqual(60, Config:get(callentry.qttl)),
+    ?assertEqual(high, Config:get(callentry.requeue.priority)).
+
