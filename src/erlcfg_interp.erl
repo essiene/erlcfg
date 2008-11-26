fbbf0be75af269173528e8b36b268ba472b540d3
Essien Ita Essien <essiene@gmail.com>
2008-11-26 12:35:48 +0100

    Debugging over... don't need this.

--- src/erlcfg_interp.erl
+++ src/erlcfg_interp.erl
@@ -23,7 +23,6 @@ eval(State, CurrentBlock, {set, Address, Value}) ->
     {NewState, CurrentBlock, Op2Value} = eval(State, CurrentBlock, Value),
 
     FullAddress = node_addr:join([CurrentBlock, Address]),
-    util:logmessage({setting, NewState, FullAddress, Op2Value}),
     case erlcfg_node:set(NewState, FullAddress, Op2Value) of
         {not_found, InvalidAddress} ->
             throw({not_found, InvalidAddress});
