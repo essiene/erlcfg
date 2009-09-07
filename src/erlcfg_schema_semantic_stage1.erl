279aa32d809918eb0dcb74d78cc82daa5adce3e7
Essien Ita Essien <essiene@gmail.com>
2009-09-07 20:12:50 +0100

    Start work on the stage1 semantic analyser
    The stage one currently just goes through the Ast and builds
    a Types proplist/tuple-list. This contains all the built in
    types, and types defined by 'type FOO = X|X|X' form.
    
    The next stage will use this to do type checking.
    
    I am aware that too many stages will slow things down, especially
    since I expect that schema checking can optionally be done
    during loading by erlcfg:new/1. I do not intend to solve this
    probably performance issue now, till everything works well, then
    I'll think of optimizing for better speed.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/erlcfg_schema_semantic_stage1.erl
@@ -0,0 +1,32 @@
+-module(erlcfg_schema_semantic_stage1).
+-include("schema.hrl").
+-export([analyse/1]).
+
+analyse([]) ->
+    [integer, float, atom, string, bool];
+analyse([Head|Rest]) ->
+    Types = [integer, float, atom, string, bool],
+    analyse(Head, Rest, Types).
+
+analyse(Current, [Head|Rest], Types0) ->
+    Types1 = eval(Current, Types0),
+    analyse(Head, Rest, Types1);
+
+analyse(Current, [], Types0) ->
+    Types1 = eval(Current, Types0),
+    Types1.
+
+eval(#typedef{name=Name, options=OptionAst}, Types) ->
+    Options = cons(OptionAst),
+    [{Name, Options}|Types];
+eval(_Other, Types) ->
+    Types.
+
+cons(#cons{}=Cons) ->
+    cons(Cons, []).
+
+cons(#cons{head=Head, tail=nil}, Accm) ->
+    lists:reverse([Head|Accm]);
+cons(#cons{head=Head, tail=Tail}, Accm) ->
+    cons(Tail, [Head|Accm]).
+
