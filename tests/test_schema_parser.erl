4477439b829b60c69d51d8f0b7917597383caaf4
Essien Ita Essien <essiene@gmail.com>
2009-09-09 23:52:14 +0100

    Continue parser testing
    * test atom and string declarations
    * test typedef statement
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- tests/test_schema_parser.erl
+++ tests/test_schema_parser.erl
@@ -19,3 +19,30 @@ parse_bool_declaration_test() ->
     Result = erlcfg_schema_parser:parse(Tokens),
     Expected = {ok, [{declaration, bool, foo}]},
     ?assertEqual(Expected, Result).
+
+parse_atom_declaration_test() ->
+    Tokens = [{datatype, 1, atom}, {atom, 1, foo}, {';', 1}],
+    Result = erlcfg_schema_parser:parse(Tokens),
+    Expected = {ok, [{declaration, atom, foo}]},
+    ?assertEqual(Expected, Result).
+
+parse_string_declaration_test() ->
+    Tokens = [{datatype, 1, string}, {atom, 1, foo}, {';', 1}],
+    Result = erlcfg_schema_parser:parse(Tokens),
+    Expected = {ok, [{declaration, string, foo}]},
+    ?assertEqual(Expected, Result).
+
+parse_typedef_test() ->
+    Tokens = [{keyword_type, 1, type}, 
+        {atom, 1, newtype},
+        {'=', 1}, 
+            {atom, 1, foo}, 
+                {'|', 1}, 
+            {atom, 1, bar}, 
+         {';', 1}
+     ],
+    Result = erlcfg_schema_parser:parse(Tokens),
+    Expected = {ok, [{typedef, newtype, {cons, foo, {cons, bar, nil}}
+            }
+    ]},
+    ?assertEqual(Expected, Result).
