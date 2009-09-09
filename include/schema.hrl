addbe691787a328a258603d39843666db2e27ebf
Essien Ita Essien <essiene@gmail.com>
2009-09-09 19:38:13 +0100

    Define #validator record
    * The validator a test/predicate and the test information,
      the predicate will eventually be run on the actual value
      during the validation run.
    
    * Also define a default validator mapping macro for the inbuilt
      data types
    
    * Update erlcfg_schema_analyser1 to use default validator mapping
      macro
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- include/schema.hrl
+++ include/schema.hrl
@@ -15,3 +15,16 @@
 -record(cons, {
         head,
         tail=nil}).
+
+-record(validator, {
+        type,
+        test
+    }).
+
+-define(DEFAULT_TYPE_MAP, [
+        {integer, #validator{type=integer, test=fun is_integer/1}},
+        {float, #validator{type=float, test=fun is_float/1}},
+        {atom, #validator{type=atom, test=fun is_atom/1}},
+        {string, #validator{type=string, test=fun is_binary/1}},
+        {boolean, #validator{type=boolean, test=fun is_boolean/1}}
+    ]).
